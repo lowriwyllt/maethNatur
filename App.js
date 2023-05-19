f68d6a3752084875fffae6dfec997fe560fd6cc5
lowriwyllt <lowriwyllt@live.co.uk>
2023-05-19 11:55:20 +0100

    Move homeScreen to its own file

--- App.js
+++ App.js
@@ -3,18 +3,7 @@ import * as React from "react";
 import { Button, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
-      />
-      <Button onPress={() => navigation.toggleDrawer()} title="sidebar" />
-    </View>
-  );
-}
+import HomeScreen from "./screens/HomeScreen";
 
 function NotificationsScreen({ navigation }) {
   return (
