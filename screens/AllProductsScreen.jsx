81b3e16c267840b330016c91f56c0aaf6befd4ea
lowriwyllt <lowriwyllt@live.co.uk>
2023-05-19 15:30:48 +0100

    Made a screen with just the title to test the navigation

---
+++ screens/AllProductsScreen.jsx
@@ -0,0 +1,9 @@
+import { Button, Text, View } from "react-native-web";
+
+export default function AllProductsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Products</Text>
+    </View>
+  );
+}
