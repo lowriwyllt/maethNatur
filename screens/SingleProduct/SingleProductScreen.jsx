8f53201463eb9f49509932e8509c981b14c8c951
lowriwyllt <lowriwyllt@live.co.uk>
2023-06-02 16:17:54 +0100

    single product page renders all the information

--- screens/SingleProduct/SingleProductScreen.jsx
+++ screens/SingleProduct/SingleProductScreen.jsx
@@ -1,8 +1,64 @@
-import { Text } from "react-native-web";
-import { useRoute } from "@react-navigation/core";
+import { Image, StyleSheet, Text } from "react-native-web";
+import { useRoute, useNavigation } from "@react-navigation/core";
+import { getProductByName } from "../../database/database";
+import { useEffect, useState, useLayoutEffect } from "react";
+import { toPounds } from "../../utils/utilFunctions";
 
 export default function SingleProductScreen() {
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const route = useRoute();
-  const productName = route.params.query;
-  return <Text>{productName}</Text>;
+  const navigation = useNavigation();
+  const productName = decodeURIComponent(route.params.query);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: productName });
+  }, [navigation, productName]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getProductByName(productName)
+      .then((response) => {
+        setProduct(response);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [productName]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text>{product.name}</Text>
+          {product.img?.map((image) => {
+            return (
+              <Image
+                key={image}
+                alt={`Image of ${product.name}`}
+                source={image}
+                style={styles.img}
+              />
+            );
+          })}
+          <Text>{product.description}</Text>
+          <Text>{product.type}</Text>
+          {product.plants?.map((plant) => {
+            return <Text key={plant}>{plant}</Text>;
+          })}
+          <Text>{toPounds(product.price)}</Text>
+        </>
+      )}
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  img: {
+    height: 50,
+    width: 50,
+  },
+});
